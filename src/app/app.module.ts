2471670ad9cc44062a0f20b649a7e6d8bf077c46
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-01-08 15:44:59 -0500

    Migrating all components to use modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { CardComponent } from "./shared/card/card.component";
+import { TaskComponent } from "./tasks/task/task.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations: [AppComponent], //declarations are used for non-standalone components
-    bootstrap: [AppComponent],
-    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent] //can be used with standalone components
+  //declarations are used for non-standalone components
+  declarations: [
+    AppComponent,
+    HeaderComponent, 
+    UserComponent, 
+    CardComponent, 
+    TasksComponent, 
+    TaskComponent, 
+    NewTaskComponent], 
+  bootstrap: [AppComponent],
+  //imports can have standalone components
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
-
