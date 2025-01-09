1506d6a84b47a78965df8eb96f2433a35dc342fe
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-01-09 10:38:36 -0500

    Create shared module for Card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   //declarations are used for non-standalone components
@@ -16,12 +16,11 @@ import { FormsModule } from "@angular/forms";
     AppComponent,
     HeaderComponent, 
     UserComponent, 
-    CardComponent, 
     TasksComponent, 
     TaskComponent, 
     NewTaskComponent], 
   bootstrap: [AppComponent],
   //imports can have standalone components
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
